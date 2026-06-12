9830058472c2c6f7f74c64850b40b5a1d56acc43
Manguianoo <manguiano4a@gmail.com>
2026-06-11 18:15:23 -0600

    tarea2: agregue botón de back

--- Tarea-2/routes/contacto.js
+++ Tarea-2/routes/contacto.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require("../controllers/contacto")
 
 router.get("/contacto", controller.showContact)
-router.post("/contacto", controller.receiveContact)
+//router.post("/contacto", controller.receiveContact) con el WebForms no lo necesitamos
 router.get("/confirmacion", controller.receiveContact)
 
 module.exports = router
