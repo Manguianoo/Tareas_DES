c3b1c5f7ec37645b206bb079314ec05f18077d0d
Manguianoo <manguiano4a@gmail.com>
2026-07-09 18:41:21 -0600

    Delete tarea4 directory

--- tarea4/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import router from "./routes";
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use("/", router);
-
-app.listen(port, () => console.log(`Servidor en puerto ${port}`));
